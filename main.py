013c2e71db842100b4ebe20499308409e40d2575
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-12-04 03:59:16 +0700

    Reformat

--- main.py
+++ main.py
@@ -12,35 +12,40 @@ app = FastAPI()
 STATIC_PATH = "/static"
 
 origins = [
-    "*"
+  "*"
 ]
+
 
 @app.on_event("startup")
 async def startup_event():
-    # await generator.init()
-    pass
+  # await generator.init()
+  pass
+
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["POST", "GET"],
-    allow_headers=["*"],
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["POST", "GET"],
+  allow_headers=["*"],
 )
+
 
 @app.get("/")
 async def read_root():
-    return {
-        "code": 200
-    }
+  return {
+    "code": 200
+  }
+
 
 def common_error(err: Exception):
-    """
-    Returns abnormal JSONResponse
-    """
-    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,
-                        content=ErrorResponse("invalid request", f"{str(err)}").dict())
+  """
+  Returns abnormal JSONResponse
+  """
+  return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,
+                      content=ErrorResponse("invalid request",
+                                            f"{str(err)}").dict())
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, log_level="info")
+  uvicorn.run("main:app", host="0.0.0.0", port=8080, log_level="info")
