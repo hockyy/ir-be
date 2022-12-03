447944f1c0f5d4d57feede4131861ce418d7d5af
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-12-04 03:46:05 +0700

    Fix req

--- main.py
+++ main.py
@@ -10,36 +10,14 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse, StreamingResponse
 
 import generator
-import os
-import redis
+from model import ErrorResponse
 
 load_dotenv()
-REDIS_HOST = os.getenv("REDIS_HOST")
-REDIS_PORT = int(os.getenv("REDIS_PORT"))
-REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
-EXPIRE_IMAGE = 60
-
-image_db = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
-
-db_dict = {
-    "image_db" : image_db
-}
-
-from model import GenshinQuery, GenshinResponse, ErrorResponse
-
 app = FastAPI()
-try:
-    os.makedirs('output')
-except:
-    pass
 STATIC_PATH = "/static"
-app.mount(STATIC_PATH, StaticFiles(directory="output"), name="static")
 
 origins = [
-    "http://genshin.hocky.id",
-    "https://genshin.hocky.id",
-    "http://localhost:3000",
-    "https://genshin-achievements-frontend.vercel.app"
+    "*"
 ]
 
 @app.on_event("startup")
@@ -68,42 +46,6 @@ def common_error(err: Exception):
     return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,
                         content=ErrorResponse("invalid request", f"{str(err)}").dict())
 
-
-@app.get("/check_db")
-async def check_db():
-    """
-    Print all values in redis database
-    """
-    all_values = dict()
-    for db_name, redis_db in db_dict.items():
-        tmp_dict = dict()
-        redis_keys = redis_db.keys()
-        for redis_key in redis_keys:
-            redis_value = redis_db.get(redis_key)
-            try:
-                redis_string = redis_value.decode("utf-8")
-            except:
-                redis_string = redis_value.hex()
-            if(not redis_string.startswith("{")):
-                redis_string = f"\"{redis_string}\""
-            # print(redis_string)
-            tmp_dict[redis_key] = json.loads(redis_string)
-        all_values[db_name] = tmp_dict
-    return all_values
-
-@app.post("/genshin", response_model=GenshinResponse)
-async def read_genshin(genshinQuery: GenshinQuery):
-    url, image = await generator.generateAchievement(genshinQuery.caption, genshinQuery.language)
-    image_db.set(url, image,ex=EXPIRE_IMAGE)
-    return GenshinResponse(f"/image/{url}", "Image succesfully generated", 200)
-
-@app.get("/image/{image_url}")
-async def read_image(image_url:str):
-    try:
-        data = image_db.get(image_url)
-        return StreamingResponse(io.BytesIO(data), media_type="image/png")
-    except Exception as e:
-        return common_error(e)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, log_level="info")
--- model.py
+++ model.py
@@ -1,27 +1,38 @@
 from typing import Any, Optional
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from typing import List
 
-class GenshinQuery(BaseModel):
-    caption: str
-    language: str
 
-class GenshinResponse(BaseModel):
-    def __init__(self, url: str, detail: str, code: int, **data: Any):
-        super().__init__(**data)
-        self.url = url
-        self.detail = detail
-        self.code = code
-    url: Optional[str] = None
-    detail: Optional[str] = None
-    code: Optional[int] = 500
+class SearchQuery(BaseModel):
+  content: str
+  rerank: bool
+
+
+class Result(BaseModel):
+  def __init__(self, code: int, **data: Any):
+    super().__init__(**data)
+    self.code = code
+
+  score: Optional[int] = 0
+  id: Optional[str] = ""
+  excerpt: Optional[str] = ""
+
+
+class SearchResponse(BaseModel):
+  def __init__(self, code: int, **data: Any):
+    super().__init__(**data)
+    self.code = code
+
+  results: Optional[List[Result]] = []
+  code: Optional[int] = 500
 
 
 class ErrorResponse(BaseModel):
-    def __init__(self, error: str, error_description: str, **data: Any):
-        super().__init__(**data)
-        self.error = error
-        self.error_description = error_description
+  def __init__(self, error: str, error_description: str, **data: Any):
+    super().__init__(**data)
+    self.error = error
+    self.error_description = error_description
 
-    error: str = "invalid_token"
-    error_description: str = "An error has occured"
+  error: str = "invalid_token"
+  error_description: str = "An error has occured"
